d8955af90ba83c6e2eaa4c914dfc556677f26071
Lenonnn <lenonpolly@gmail.com>
2021-12-15 11:48:03 -0300

    Fisrt Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+// ctrl + SHIFT + /  PARA COMENTAR OU DESCOMENTAR
+// ctrl + ALT + O    PARA APAGAR IMPORTS NÃO USADOS
+// ALT + ENTER       PARA FAZER AUTO IMPORT
+// ctrl + Y          PARA deletar linha
+// ctrl + D          PARA duplicar linha
+// ctrl + alt + L    PARA identação de código
+// ctrl + shift + seta PARA Mover linha
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*
+        int a = 5;
+        int b = 3;
+        System.out.println("Hello World " + ( a + b )) ;
+        */
+    }
+}
+

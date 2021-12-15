8510090a3f42c41e80d7c8c8edc921e1b0e8908b
Lenonnn <lenonpolly@gmail.com>
2021-12-15 12:07:13 -0300

    Criação de class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -18,11 +18,51 @@ public class PrimeiroPrograma {
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Os três mosqueteiros",300);
+        Livro livro2 = new Livro("Minha primeria viagem",280);
+        Livro livro3 = new Livro("Minha primeira Visita",500);
+
+        System.out.println(livro1 + "\n" + livro2 + "\n" + livro3);
+
         /*
         int a = 5;
         int b = 3;
         System.out.println("Hello World " + ( a + b )) ;
         */
+    }
+}
+
+class  Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
 
